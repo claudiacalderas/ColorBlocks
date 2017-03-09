392e0eab21c18fde3179c9ddacdc9643b0ebc16d
Claudia Calderas <claudia.calderas@gmail.com>
2017-03-08 21:08:32 -0600

    added code to update counters and display them on the page

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
 console.log("Test sourced");
 
+var redCounterVar=0;
+var blueCounterVar=0;
+var greenCounterVar=0;
+var yellowCounterVar=0;
+
 function onReady(){
   console.log("Document ready");
 
@@ -12,7 +17,6 @@ function onReady(){
   $('#redButton').click(function() {
     buttonClickFunc("red");
   });
-
   $('#blueButton').click(function() {
     buttonClickFunc("blue");
   });
@@ -28,27 +32,49 @@ function onReady(){
 function addBlock(color) {
   var divString = "";
 
+  // builds HTML <div> element and appends it to the <body>
   divString = '<div class ="block ' + color +'"></div>';
-  console.log(divString);
   $('body').append(divString);
+
+  // increases counter
+  switch (color) {
+    case "red":
+      redCounterVar+=1;
+      break;
+    case "blue":
+      blueCounterVar+=1;
+      break;
+    case "green":
+      greenCounterVar+=1;
+      break;
+    case "yellow":
+      yellowCounterVar+=1;
+      break;
+  }
+  updateCounter(color);
 }
 
-function buttonClickFunc(color) {
+// Updates the counters on the page
+function updateCounter(color){
     switch (color) {
-      case "red":
-        addBlock(color);
-        break;
-      case "blue":
-        addBlock(color);
-        break;
-      case "green":
-        addBlock(color);
-        break;
-      case "yellow":
-        addBlock(color);
-        break;
-      default:
-    }
+    case "red":
+      $(".redCounter").text("Red count: " + redCounterVar);
+      break;
+    case "blue":
+      $(".blueCounter").text("Blue count: " + blueCounterVar);
+      break;
+    case "green":
+      $(".greenCounter").text("Green count: " + greenCounterVar);
+      break;
+    case "yellow":
+      $(".yellowCounter").text("Yellow count: " + yellowCounterVar);
+      break;
+  }
+}
+
+// calls function to add block and increase counter depending on color
+function buttonClickFunc(color) {
+  addBlock(color);
 }
 
 $(document).ready(onReady);
