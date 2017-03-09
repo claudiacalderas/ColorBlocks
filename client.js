4613e39e74d1374b1080107e9800200b7c8e1a92
Claudia Calderas <claudia.calderas@gmail.com>
2017-03-08 20:38:46 -0600

    added code to create blocks when clicking buttons

--- client.js
+++ client.js
@@ -8,6 +8,20 @@ function onReady(){
   addBlock("green");
   addBlock("yellow");
 
+  // event listeners
+  $('#redButton').click(function() {
+    buttonClickFunc("red");
+  });
+
+  $('#blueButton').click(function() {
+    buttonClickFunc("blue");
+  });
+  $('#greenButton').click(function() {
+    buttonClickFunc("green");
+  });
+  $('#yellowButton').click(function() {
+    buttonClickFunc("yellow");
+  });
 }
 
 // adds blocks to the DOM
@@ -17,7 +31,24 @@ function addBlock(color) {
   divString = '<div class ="block ' + color +'"></div>';
   console.log(divString);
   $('body').append(divString);
+}
 
+function buttonClickFunc(color) {
+    switch (color) {
+      case "red":
+        addBlock(color);
+        break;
+      case "blue":
+        addBlock(color);
+        break;
+      case "green":
+        addBlock(color);
+        break;
+      case "yellow":
+        addBlock(color);
+        break;
+      default:
+    }
 }
 
 $(document).ready(onReady);
